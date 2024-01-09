e10155d4ac03520ff6390f53effbaf857df0d588
Asish <asishkumar717@gmail.com>
2024-01-09 11:34:41 +0530

    Added: Adding user registration with custom exception

--- src/main/java/com/java/UserRegistration.java
+++ src/main/java/com/java/UserRegistration.java
@@ -4,25 +4,50 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
+    public class InvalidUserException extends Exception{
+        InvalidUserException(String s){
+            super(s);
+        }
+    }
     public boolean userFirstName(){
         Pattern p= Pattern.compile("[A-Z]{1}[a-z]{2,}");
         Matcher m=p.matcher("Asish");
         return m.matches();
     }
-    public boolean userLastName(){
-        return Pattern.compile("[A-Z]{1}[a-z]{2,}").matcher("Dash").matches();
+    public boolean userLastName() throws InvalidUserException{
+        if (Pattern.compile("[A-Z]{1}[a-z]{2,}").matcher("Dash").matches()){
+            return true;
+        }
+        else {
+            throw new InvalidUserException("Invalid last name");
+        }
         //return lastName.matches("[A-Z]{1}[a-z]{2,}");
     }
-    public boolean email(){
-        return Pattern.compile("[a-zA-Z0-9._&*]+@[a-z]{2,}\\.[a-z]{2,}")
-                .matcher("asish717@gmail.com").matches();
+    public boolean email() throws InvalidUserException{
+        if (Pattern.compile("[a-zA-Z0-9._&*]+@[a-z]{2,}\\.[a-z]{2,}")
+                .matcher("asish717@gmail.com").matches()){
+            return true;
+        }
+        else {
+            throw new InvalidUserException("Invalid email");
+        }
     }
-    public boolean phoneNumber(){
-        return Pattern.compile("[0-9]{1,3} [0-9]{10}")
-                .matcher("91 8297760717").matches();
+    public boolean phoneNumber()throws InvalidUserException{
+        if (Pattern.compile("[0-9]{1,3} [0-9]{10}")
+                .matcher("91 8297760717").matches()){
+            return true;
+        }
+        else {
+            throw new InvalidUserException("Invalid phone number");
+        }
     }
-    public boolean userPassword(){
-        return Pattern.compile("(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*]).{8,}").matcher("kumar1Dash#").matches();
+    public boolean userPassword() throws InvalidUserException{
+        if (Pattern.compile("(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*]).{8,}").matcher("kumar1Dash#").matches()){
+            return true;
+        }
+        else {
+            throw new InvalidUserException("Invalid password");
+        }
     }
     public boolean emailSample(String email){
         boolean isValidEmail=email.matches("[a-zA-Z0-9._&*+-]+@[a-z0-9.]{1,}\\.[a-z]{2,}");
--- src/test/java/com/java/UserRegistrationTest.java
+++ src/test/java/com/java/UserRegistrationTest.java
@@ -13,26 +13,47 @@ public class UserRegistrationTest {
     }
     @Test
     public void nameLast(){
-        boolean check=user.userLastName();
-        System.out.println(check);
-        Assert.assertTrue(check);
+        try {
+            boolean check=user.userLastName();
+            System.out.println(check);
+            Assert.assertTrue(check);
+        }
+        catch (UserRegistration.InvalidUserException e){
+            throw new RuntimeException(e);
+        }
+
     }
     @Test
     public void userEmail(){
-        boolean check=user.email();
-        System.out.println(check);
-        Assert.assertTrue(check);
+        try {
+            boolean check=user.email();
+            System.out.println(check);
+            Assert.assertTrue(check);
+        }
+        catch (UserRegistration.InvalidUserException e){
+            throw new RuntimeException(e);
+        }
     }
     @Test
     public void phone(){
-        boolean check=user.phoneNumber();
-        System.out.println(check);
-        Assert.assertTrue(check);
+        try {
+            boolean check=user.phoneNumber();
+            System.out.println(check);
+            Assert.assertTrue(check);
+        }
+        catch (UserRegistration.InvalidUserException e){
+            throw new RuntimeException(e);
+        }
     }
     @Test
     public void password(){
-        boolean check=user.userPassword();
-        System.out.println(check);
-        Assert.assertTrue(check);
+        try {
+            boolean check=user.userPassword();
+            System.out.println(check);
+            Assert.assertTrue(check);
+        }
+        catch (UserRegistration.InvalidUserException e){
+            throw new RuntimeException(e);
+        }
     }
 }
